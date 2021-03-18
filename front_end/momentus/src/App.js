9f7d49c31e782a188ce4d383c2f5859ad31d657d
Matt Bolles <mattrbolles@gmail.com>
2021-03-17 19:37:40 -0700

    Front end and back end set up and connected

--- front_end/momentus/src/App.js
+++ front_end/momentus/src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
+
+
 
 function App() {
+  // connects front end to back
+  Axios({
+    method: "GET",
+    url: "http://localhost:3306/prototype",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => {
+    console.log(res.data.message);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test!
+          test! - front end stuff will go here
         </p>
         <a
           className="App-link"
