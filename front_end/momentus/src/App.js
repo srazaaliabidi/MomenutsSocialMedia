964db1e9808f7086e48fe9ca0ff24ad8f4068adb
Natalie Christie <email@mail.sfsu.edu>
2021-03-31 13:08:31 -0700

    Added Routes + CSS

--- front_end/momentus/src/App.js
+++ front_end/momentus/src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
 import About from './pages/about';
+import Messages from './pages/messages';
+import Notifs from './pages/notifs';
+import Settings from './pages/settings';
+import Search from './pages/search';
 
 function App() {
   // used to display loading text
@@ -24,6 +28,18 @@ function App() {
           </Route>
           <Route path="/about">
             <About />
+          </Route>
+          <Route path="/messages">
+            <Messages />
+          </Route>
+          <Route path="/notifs">
+            <Notifs />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+          <Route path="/search">
+            <Search />
           </Route>
         </Switch>
     </BrowserRouter>
