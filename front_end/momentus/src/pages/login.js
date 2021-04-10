969481955f0416a377d9c24c995b004b7ff56b83
Jo Ey <cloud.joey98@gmail.com>
2021-04-10 16:44:33 +0800

    added react state, data handling

--- front_end/momentus/src/pages/login.js
+++ front_end/momentus/src/pages/login.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import './styles/login.css';
+import React, { useState } from 'react';
+import './styles/login-reg.css';
 import logo from '../assets/momentuslogo.png';
 
-function Login () {
+function Login() {
+  const [username, setUsername] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = (e) => {
+    
+  }
+
   return (
-    <div className="login">
-      <img src={logo} />
-      <form id="userlogin">
-        <div className="input-box">
-          <input type="text" placeholder="Username" /><br />
-          <input type="text" placeholder="Email" /><br />
-          <input type="password" placeholder="Password" /><br />
-          <input type="password" placeholder="Confirm Password" /><br />
-        </div>
-        <input type="submit" className="submitlogin">Log in</input><br />
-      </form><br />
-      <a href="/register">New? Create an account!</a><br />
+    <div className="login-reg-wrapper">
+      <div className="login-reg-box">
+        <img src={logo} />
+        <h1>Welcome Back!</h1>
+        <form id="/userlogin">
+          <div className="input-box">
+            <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} /><br />
+            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} /><br />
+            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} /><br />
+          </div>
+          <input type="submit" className="submit-login" value="Login" /><br />
+        </form><br />
+        <a href="/register">New? Create an account!</a><br />
+        <p>Data testing</p>
+        <p>username: {username}</p>
+        <p>email: {email}</p>
+        <p>password: {password}</p>
+      </div>
     </div>
   );
 }
