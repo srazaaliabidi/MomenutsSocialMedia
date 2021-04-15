7e7a95f7be178fbc1d8d7e3e1d0daf74bc100174
Jo Ey <cloud.joey98@gmail.com>
2021-04-16 01:07:17 +0800

    settings page styles and basic layout added

--- front_end/momentus/src/pages/settings.js
+++ front_end/momentus/src/pages/settings.js
@@ -1,11 +1,33 @@
 import React from 'react';
-// import '../App.css';
+import './styles/settings.css'
 
-function Settings () {
+function Settings() {
+  const [settingsForm, setSettingsForm] = React.useState({
+    usernameChange: '',
+    emailChange: '',
+    passwordChange: '',
+    passChangeConfirm: ''
+  })
+
+  const handleChange = (e) => {
+    setSettingsForm({
+      [e.target.name]: [e.target.value]
+    })
+  }
+
   return (
     <div class="centergrid">
-      <div>
-        <h1>[Settings]</h1>
+      <div className="settings-container">
+        <form>
+          <label for="usernameChange">Change Username</label>
+          <input type="text" id="usernameChange" placeholder="Username" onChange={handleChange} />
+          <label for="emailChange">Change Email</label>
+          <input type="email" id="emailChange" placeholder="Email" onChange={handleChange} />
+          <label for="passwordChange">Change Password</label>
+          <input type="text" id="passwordChange" placeholder="Username" onChange={handleChange} />
+          <label for="passChangeConfirm">Confirm Password</label>
+          <input type="text" id="passConfirmChange" placeholder="Username" onChange={handleChange} />
+        </form>
       </div>
     </div>
   );
