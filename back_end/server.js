4d6da1f959ce616306c14cd6dc9364d43c895992
Matt Bolles <mattrbolles@gmail.com>
2021-04-18 19:12:35 -0700

    Server.js modification for heroku

--- back_end/server.js
+++ back_end/server.js
@@ -18,6 +18,14 @@ app.use (
   session ({secret: 'CSC648csc!', resave: false, saveUninitialized: false})
 );
 
+// to allow Heroku to load routes properly
+app.get('/*', (req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/')) 
+    url = url.substring(1);
+  res.sendFile(url);
+});
+
 /*----------------------Prepare---------------------------*/
 
 connectSql ();
