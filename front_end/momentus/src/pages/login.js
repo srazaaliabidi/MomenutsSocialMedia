f0bd0fa61d15a1008bf8a6ab8333fd5497c019fa
Jo Ey <cloud.joey98@gmail.com>
2021-04-27 16:36:50 +0800

    fixed div issues

--- front_end/momentus/src/pages/login.js
+++ front_end/momentus/src/pages/login.js
@@ -1,60 +1,58 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './styles/login-reg.css';
 import logo from '../assets/momentuslogo.png';
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import axios from 'axios';
-import {userLogin} from '../redux/actions/loginActions';
+import { userLogin } from '../redux/actions/loginActions';
 import './styles/home.css';
 
-function Login () {
-  const dispatch = useDispatch ();
-  const [usernameToSubmit, setUsername] = useState ('');
-  const [passwordToSubmit, setPassword] = useState ('');
+function Login() {
+  const dispatch = useDispatch();
+  const [usernameToSubmit, setUsername] = useState('');
+  const [passwordToSubmit, setPassword] = useState('');
   let username = '';
   let userID = '';
   let userData = {};
 
 
-  function submit(e){
-  e.preventDefault();
-    axios.post('/verifyUser',{
-        usernameToSubmit:usernameToSubmit,
-        passwordToSubmit:passwordToSubmit,
+  function submit(e) {
+    e.preventDefault();
+    axios.post('/verifyUser', {
+      usernameToSubmit: usernameToSubmit,
+      passwordToSubmit: passwordToSubmit,
     })
-    .then(response=>{
-    console.log("Logged in")
-    })
+      .then(response => {
+        console.log("Logged in")
+      })
   }
 
 
 
   return (
-    <div className="centergrid">
-      <div className="login-reg-wrapper">
-        <div className="login-reg-box">
-          <img src={logo} />
-          <h1>Welcome Back!</h1>
-          <form id="/userlogin" onSubmit = {(e)=>submit(e)}>
-            <div className="input-box">
-              <input
-                type="text"
-                placeholder="Username"
-                value={usernameToSubmit}
-                onChange={e => setUsername (e.target.value)}
-              />
-              <br />
-              <input
-                type="password"
-                placeholder="Password"
-                value = {passwordToSubmit}
-                onChange={e => setPassword (e.target.value)}
-              />
-              <br />
-            </div>
-            <input type="submit" className="submit-login" value="Login" /><br />
-          </form><br />
-          <a href="/register">New? Create an account!</a><br />
-        </div>
+    <div className="login-reg-wrapper">
+      <div className="login-reg-box">
+        <img src={logo} />
+        <h1>Welcome Back!</h1>
+        <form id="/userlogin" onSubmit={(e) => submit(e)}>
+          <div className="input-box">
+            <input
+              type="text"
+              placeholder="Username"
+              value={usernameToSubmit}
+              onChange={e => setUsername(e.target.value)}
+            />
+            <br />
+            <input
+              type="password"
+              placeholder="Password"
+              value={passwordToSubmit}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <br />
+          </div>
+          <input type="submit" className="submit-login" value="Login" /><br />
+        </form><br />
+        <a href="/register">New? Create an account!</a><br />
       </div>
     </div>
   );
--- front_end/momentus/src/pages/register.js
+++ front_end/momentus/src/pages/register.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/login-reg.css';
 import logo from '../assets/momentuslogo.png';
 import './styles/home.css';
@@ -6,40 +6,41 @@ import axios from "axios";
 
 function Register() {
   const [form, setForm] = useState({
-      email: '',
-      username: '',
-      password: '',
-      confirmPass: '',
-      firstName: '',
-      lastName: '',
-      city: '',
-      state: '',
-      birthdate: ''});
+    email: '',
+    username: '',
+    password: '',
+    confirmPass: '',
+    firstName: '',
+    lastName: '',
+    city: '',
+    state: '',
+    birthdate: ''
+  });
   const [step, setStep] = useState(1);
 
 
-  function submit(e){
-   e.preventDefault();
-     axios.post('/newUser',{
-        email:form.email,
-        username:form.username,
-        password:form.password,
-        firstName:form.firstName,
-        lastName:form.lastName,
-        city:form.city,
-        state:form.state,
-        birthdate:form.birthdate
+  function submit(e) {
+    e.preventDefault();
+    axios.post('/newUser', {
+      email: form.email,
+      username: form.username,
+      password: form.password,
+      firstName: form.firstName,
+      lastName: form.lastName,
+      city: form.city,
+      state: form.state,
+      birthdate: form.birthdate
     })
-    .then(result=>{
-    console.log(result.data)
-    })
+      .then(result => {
+        console.log(result.data)
+      })
   }
 
-  function updateForm(e){
-  const formdata={...form}
-  formdata[e.target.name]=e.target.value
-  setForm(formdata)
-  console.log(formdata)
+  function updateForm(e) {
+    const formdata = { ...form }
+    formdata[e.target.name] = e.target.value
+    setForm(formdata)
+    console.log(formdata)
   }
   /*const updateForm = (e) => {
     setForm({
@@ -57,112 +58,111 @@ try {
     }
 */
   return (
-    <div className="centergrid">
-      <div className="login-reg-wrapper">
-        <div className="login-reg-box">
-          <img src={logo} />
-          <h1>Create Account</h1>
-          <div className="input-box">
-            <form
-             id="/userregister"
-             action="back_end/server/createUser"
-             method="POST"
-             onSubmit={(e)=>submit(e)}
-            >
-              {step === 1 ? (
-                <div>
-                  <input
-                    type="email"
-                    value={form.email}
-                    name="email"
-                    placeholder="Email"
-                    onChange={(e)=>updateForm(e)}
-                  />
-                  <br />
-                  <input
-                    type="text"
-                    value={form.username}
-                    name="username"
-                    placeholder="Username"
-                    onChange={(e)=>updateForm(e)}
-                  />
-                  <br />
-                  <input
-                    type="password"
-                    value={form.password}
-                    name="password"
-                    placeholder="Password"
-                    onChange={(e)=>updateForm(e)}
-                  />
-                  <br />
-                  <input
-                    type="password"
-                    name="confirmPass"
-                    placeholder="Confirm Password"
-                    id="confirm-password"
-                    onChange={(e)=>updateForm(e)}
-                  />
-                </div>
-              ) : null}
+    <div className="login-reg-wrapper">
+      <div className="login-reg-box">
+        <img src={logo} />
+        <h1>Create Account</h1>
+        <div className="input-box">
+          <form
+            id="/userregister"
+            action="back_end/server/createUser"
+            method="POST"
+            onSubmit={(e) => submit(e)}
+          >
+            {step === 1 ? (
+              <div>
+                <input
+                  type="email"
+                  value={form.email}
+                  name="email"
+                  placeholder="Email"
+                  onChange={(e) => updateForm(e)}
+                />
+                <br />
+                <input
+                  type="text"
+                  value={form.username}
+                  name="username"
+                  placeholder="Username"
+                  onChange={(e) => updateForm(e)}
+                />
+                <br />
+                <input
+                  type="password"
+                  value={form.password}
+                  name="password"
+                  placeholder="Password"
+                  onChange={(e) => updateForm(e)}
+                />
+                <br />
+                <input
+                  type="password"
+                  name="confirmPass"
+                  placeholder="Confirm Password"
+                  id="confirm-password"
+                  onChange={(e) => updateForm(e)}
+                />
+              </div>
+            ) : null}
 
-              {step === 2 ? (
-                <div>
-                  <input
-                    type="text"
-                    value={form.firstName}
-                    name="firstName"
-                    placeholder="First Name"
-                    onChange={(e)=>updateForm(e)}
-                  />
-                  <br />
-                  <input
-                    type="text"
-                    value={form.lastName}
-                    name="lastName"
-                    placeholder="Last Name"
-                    onChange={(e)=>updateForm(e)}
-                  />
-                  <br />
-                  <input
-                    type="text"
-                    value={form.city}
-                    name="city"
-                    placeholder="City"
-                    id="city"
-                    onChange={(e)=>updateForm(e)}
-                  />
-                  <br />
-                  <input
-                    type="text"
-                    value={form.state}
-                    name="state"
-                    placeholder="State"
-                    id="state"
-                    onChange={(e)=>updateForm(e)}
-                  />
-                  <br />
-                  <label htmlFor="DOB">Date of birth</label><br />
-                  <input
-                    type="date"
-                    value={form.birthdate}
-                    name="birthdate"
-                    placeholder="Date of Birth"
-                    id="DOB"
-                    onChange={(e)=>updateForm(e)}
-                  />
-                  <br />
-                  <input type="submit" value="Register" /><br />
-                </div>
-              ) : null}
-              {step === 1 ? (
-                <button type="button" onClick={() => setStep(step + 1)}>Next</button>
-              ) : null}
-            </form>
-            <br />
-          </div>
-          <a href="/login">Log in instead</a><br />
-          
-          {/* 
+            {step === 2 ? (
+              <div>
+                <input
+                  type="text"
+                  value={form.firstName}
+                  name="firstName"
+                  placeholder="First Name"
+                  onChange={(e) => updateForm(e)}
+                />
+                <br />
+                <input
+                  type="text"
+                  value={form.lastName}
+                  name="lastName"
+                  placeholder="Last Name"
+                  onChange={(e) => updateForm(e)}
+                />
+                <br />
+                <input
+                  type="text"
+                  value={form.city}
+                  name="city"
+                  placeholder="City"
+                  id="city"
+                  onChange={(e) => updateForm(e)}
+                />
+                <br />
+                <input
+                  type="text"
+                  value={form.state}
+                  name="state"
+                  placeholder="State"
+                  id="state"
+                  onChange={(e) => updateForm(e)}
+                />
+                <br />
+                <label htmlFor="DOB">Date of birth</label><br />
+                <input
+                  type="date"
+                  value={form.birthdate}
+                  name="birthdate"
+                  placeholder="Date of Birth"
+                  id="DOB"
+                  onChange={(e) => updateForm(e)}
+                />
+                <br />
+                <input type="submit" value="Register" /><br />
+              </div>
+            ) : null}
+            {step === 1 ? (
+              <button type="button" onClick={() => setStep(step + 1)}>Next</button>
+            ) : null}
+          </form>
+          <br />
+        </div>
+        <a href="/login">Log in instead</a><br />
+
+        {/* 
           ------- Data check -------
           <p>email: {form.email}</p>
           <p>username: {form.username}</p>
@@ -174,7 +174,6 @@ try {
           <p>state: {form.state}</p>
           <p>DOB: {form.birthdate}</p>
           */}
-        </div>
       </div>
     </div>
   );
