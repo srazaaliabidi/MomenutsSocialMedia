a6588652ff29ea9de603e2ce31435352d84fe926
Matt Bolles <mattrbolles@gmail.com>
2021-05-20 17:17:24 -0700

    trying to get images to load in heroku

--- back_end/server.js
+++ back_end/server.js
@@ -20,6 +20,7 @@ const cors = require('cors');
 
 if (process.env.NODE_ENV === "production") {
 	app.use (express.static (path.resolve (__dirname, '../front_end/momentus/build')));
+	app.use(express.static(path.join(__dirname, 'public')));
 }
 app.use (
 	session ({secret: 'CSC648csc!', resave: false, saveUninitialized: false})
